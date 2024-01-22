1b4eb127ddf0faf43661c4c58ea7f6ae9706598e
Jhantu Das <107881406+jhantu07@users.noreply.github.com>
2024-01-22 20:02:06 +0000

    controler config done

---
+++ basics/variable.js
@@ -0,0 +1,7 @@
+const account_id=144
+let account_email="jhantu@gmail.com"
+var account_pas="1223"
+account_city="kolkata"
+/*console.log(account_id);*/
+let account_state
+console.table([account_id,account_email,account_pas,account_city,account_state]);
